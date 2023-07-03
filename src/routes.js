9c683b087813b21a7080e36a179c4493fda72721
davidgoulart-dev <davidgoulart.tae@gmail.com>
2023-07-03 19:16:03 -0300

    Criando rotas da API depois de conectá-la ao mysql

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const CarroController = require('./controllers/CarroControler')
+
+
+
+
+router.get('/carros ')
+
+
+
+module.exports = router;
